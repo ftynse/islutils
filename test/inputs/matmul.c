be32b470c735839cf739774f0671978de3da7eda
Lorenzo Chelini <l.chelini@tue.nl>
2018-08-10 15:01:32 +0200

    matmul test

---
+++ test/inputs/matmul.c
@@ -0,0 +1,17 @@
+
+#define N 1024
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+int main(){
+  #pragma scop
+  for(int i=0; i<N; ++i)
+    for(int j=0; j<N; ++j) {
+      C[i][j] = 0;
+      for(int k=0; k<N; ++k)
+        C[i][j] += B[k][j] * A[i][k];
+    }
+  #pragma endscop
+}
